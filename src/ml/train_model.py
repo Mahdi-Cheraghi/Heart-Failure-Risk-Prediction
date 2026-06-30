294347516f8cb73698330fbfa67467c54c1d5eea
Mahdi Cheraghi <mahdicheraghi.info@gmail.com>
2026-06-30 14:16:10 +0330

    chore: setup project dependencies

---
+++ src/ml/train_model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import joblib
+from pathlib import Path
+
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
