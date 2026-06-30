88b4a10e60afa5f837e905685c264dcb1cc8998e
Mahdi Cheraghi <mahdicheraghi.info@gmail.com>
2026-06-30 18:26:57 +0330

    chore: initialize Flask app with template and static paths

--- src/api/app.py
+++ src/api/app.py
@@ -2,3 +2,9 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import joblib
 from pathlib import Path
+
+app = Flask(
+    __name__,
+    template_folder="../templates",
+    static_folder="../static"
+)
