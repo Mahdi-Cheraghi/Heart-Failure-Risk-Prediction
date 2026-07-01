65ac0b046d5fe0dea4c596f0c5b46770a9a6e31f
Mahdi Cheraghi <mahdicheraghi.info@gmail.com>
2026-07-01 09:10:23 +0330

    feat: add heart form request handler

--- src/static/script.js
+++ src/static/script.js
@@ -22,3 +22,37 @@ document.addEventListener("DOMContentLoaded", function () {
         mainContent.classList.remove("hidden");
     }
 });
+
+document
+    .getElementById("heartForm")
+    .addEventListener("submit", async function (e) {
+        e.preventDefault();
+
+        const resultBox = document.getElementById("result");
+
+        // show loading
+        resultBox.classList.remove("hidden");
+        resultBox.innerHTML = "Loading...";
+
+        const data = {
+            age: Number(document.getElementById("age").value),
+            sex: Number(document.getElementById("sex").value),
+            anaemia: Number(document.getElementById("anaemia").value),
+            diabetes: Number(document.getElementById("diabetes").value),
+            high_blood_pressure: Number(
+                document.getElementById("high_blood_pressure").value,
+            ),
+            smoking: Number(document.getElementById("smoking").value),
+            creatinine_phosphokinase: Number(
+                document.getElementById("creatinine_phosphokinase").value,
+            ),
+            ejection_fraction: Number(
+                document.getElementById("ejection_fraction").value,
+            ),
+            platelets: Number(document.getElementById("platelets").value),
+            serum_creatinine: Number(
+                document.getElementById("serum_creatinine").value,
+            ),
+            serum_sodium: Number(document.getElementById("serum_sodium").value),
+        };
+    });
